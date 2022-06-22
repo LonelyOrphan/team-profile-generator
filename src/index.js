70ddffcb5d2069ba8d5b261051df59a338847072
Natasha Beddow <beddow90@gmail.com>
2022-06-22 11:48:15 +0100

    started questions program

--- src/index.js
+++ src/index.js
@@ -2,60 +2,49 @@
 import fs from "fs";
 import inquirer from "inquirer";
 import generateHtml from "./utils/generateHtml.js";
+import Employee from "./lib/Employee";
+import Manager from "./lib/Manager";
+import Engineer from "./lib/Engineer";
+import Intern from "./lib/Intern";
 
-// Questions for user input
-const questions = () => {
+const home = () => {
   return inquirer.prompt([
     {
-      type: "input",
-      message: "What is your project title?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Enter your project description",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "How does the user install your app?",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "Enter the usage instructions for this app",
-      name: "usage",
-    },
-    {
       type: "list",
-      message: "What license(s) is this app using?",
-      choices: ["MIT", "Apache", "GNU"],
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "How can people contribute to this project?",
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message: "How do you run tests on this app?",
-      name: "tests",
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your github username?",
-      name: "github",
+      message:
+        "Welcome to the team profile generator. What would you like to do?",
+      choices: [
+        "Add a Manager",
+        "Add an Engineer",
+        "Add an Intern",
+        "Exit and build webpage",
+      ],
+      name: "home",
     },
   ]);
 };
 
-// Write to README file
+const manager = () => {
+  return inquirer.createPromptModule([
+    {
+      type: "input",
+      message: "Enter the Manager's name:",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "Enter the manager's employee ID:",
+      name: "managerId",
+    },
+  ]);
+};
+
+// Questions for user input
+const questions = () => {
+  home().then((choice) => {});
+};
+
+// Write to index.html file
 const saveWebpage = (data) => {
   console.log(data);
   fs.writeFile("index.html", data, function (err) {
--- src/utils/generateHtml.js
+++ src/utils/generateHtml.js
@@ -5,5 +5,3 @@ export default function generateHtml(data) {
     `
   );
 }
-
-// module.exports = generateHtml;
