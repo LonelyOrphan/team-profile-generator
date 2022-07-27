60d8fa1b8bfab6824b4d90b8c29394b5c33fead3
Natasha Beddow <beddow90@gmail.com>
2022-07-27 16:19:26 +0100

    .

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Include packages needed for this application
 import fs from "fs";
 import inquirer from "inquirer";
-import generateHtml from "./utils/generateHtml.js";
-import Employee from "./lib/Employee.js";
-import Manager from "./lib/Manager.js";
-import Engineer from "./lib/Engineer.js";
-import Intern from "./lib/Intern.js";
+import generateHtml from "./src/utils/generateHtml.js";
+// import Employee from "./src/lib/Employee.js";
+import Manager from "./src/lib/Manager.js";
+import Engineer from "./src/lib/Engineer.js";
+import Intern from "./src/lib/Intern.js";
 
 export const employeeRoster = [];
 
